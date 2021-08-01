92c0b2527ec0001d6a3bac1184e3c4bb9403e139
BmF10 <bimafebriansyah1002@gmail.com>
2021-08-01 12:05:10 +0700

    dashboard hello

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,4 +1,4 @@
-<h1>Hello World</h1>
+<h2><span style="font-weight: bold;">Selamat Datang,</span> <?= get_session('nama') ?></h2>
 <script>
 	$(document).ready(function() {
 		$('#dashboard_menu').addClass('active')
